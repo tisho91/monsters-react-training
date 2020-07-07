3b11cbe7db3d15ec0a22f6e84412af0c44596dd5
Tihomir Marinov <tihomir.j.marinov@gmail.com>
2020-07-07 14:14:34 +0300

    Updates

---
+++ precache-manifest.eda4c23f5c6149b9523e41682eec2079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c697cb3f5875498ec01bc043e5ed966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0771ce776ab47ebf13b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2a8d18c60ae01bb7221",
+    "url": "/static/js/2.ebf292fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ebf292fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0771ce776ab47ebf13b",
+    "url": "/static/js/main.f77060c2.chunk.js"
+  },
+  {
+    "revision": "6ee33a9eb5113d8c4c15",
+    "url": "/static/js/runtime-main.52709719.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
